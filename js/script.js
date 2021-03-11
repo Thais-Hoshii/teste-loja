ffaea941b2c27dca93618aa3adb0b6835e8408e3
Thais Hoshii <53278565+Thais-Hoshii@user.noreply.github.com>
2021-03-10 23:37:36 -0300

    Change margin for banner and transfer code
    Changed the #nav-menu position to relative, so it would not cover the main content. Changed the margin for the banner and the search bar in the index page to keep the distance between each other. Transferred the code from the index to the script.js, as it will be used for all pages.

--- js/script.js
+++ js/script.js
@@ -1,11 +1,28 @@
 "use strict"
-
+// Atalho para selecionar um conjunto de elementos
 function $(txt) { return document.querySelectorAll(txt); }
 
-window.onload = checkViewportWidth;
-window.onresize =  checkViewportWidth;
+// Quando iniciar a página ou mudar comprimento da tela, usar função
+window.onload = toggleNavBar;
+window.onresize = toggleNavBar;
 
-function checkViewportWidth() {
+$("#menu-btn")[0].addEventListener("click", () => { return openTabs(".nav-item"); });
+$(".nav-item").forEach( (x, i) => {
+    let n = $(".dropitem")[i];
+    x.addEventListener("click",  () => {
+        openTabs(".open-icon", i);
+        openTabs(".close-icon", i);
+        return openTabs(".dropitem", i);
+    });
+    x.addEventListener("focusout", () => {
+        if (!n.classList.contains("hidden")) {
+            n.classList.add("hidden");
+        }
+    });
+})
+
+// Função para ligar/desligar barra de navegação para telas com width >= 600px
+function toggleNavBar() {
     let navItems = $(".nav-item");
     let attr = "hidden";
 
@@ -24,6 +41,7 @@ function checkViewportWidth() {
     }
 }
 
+// Função para ocultar ou mostrar um elemento
 function openTabs(name, i) {
     let arr = $(name);
     let c = "hidden";
